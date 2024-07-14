b2451514cd3426eb50cf874221433dfd68f800f4
Barlas Söker <bsoker98@gmail.com>
2024-07-14 20:44:50 +0300

    Added the logic to handle user actions on the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import http from "http";
 import "dotenv/config";
+import mongoose from "mongoose";
+import { logger } from "./loggers/logger";
 
 const app = express();
 const server = http.createServer(app);
+mongoose
+  .connect(process.env.DB_URI!)
+  .then(() => logger.info(`Connected to ${process.env.DB_URI}`));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
